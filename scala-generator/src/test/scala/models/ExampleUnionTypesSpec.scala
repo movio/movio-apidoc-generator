1c3aea92ea30deb19a638770d617281012d2806a
kalmanb <kalman@movio.co>
2016-03-03 16:48:12 +1300

    Remove ning and play 2.2 and 2.3

--- scala-generator/src/test/scala/models/ExampleUnionTypesSpec.scala
+++ scala-generator/src/test/scala/models/ExampleUnionTypesSpec.scala
@@ -1,7 +1,6 @@
 package scala.models
 
 import com.bryzek.apidoc.generator.v0.models.InvocationForm
-import ning.Ning18ClientGenerator
 import org.scalatest.{ShouldMatchers, FunSpec}
 
 class ExampleUnionTypesSpec extends FunSpec with ShouldMatchers {
@@ -9,21 +8,11 @@ class ExampleUnionTypesSpec extends FunSpec with ShouldMatchers {
   private lazy val service = models.TestHelper.parseFile(s"/examples/union-types-service.json")
 
   it("generates expected code for play 2.3 client") {
-    Play23ClientGenerator.invoke(InvocationForm(service = service)) match {
+    Play24ClientGenerator.invoke(InvocationForm(service = service)) match {
       case Left(errors) => fail(errors.mkString(", "))
       case Right(sourceFiles) => {
         sourceFiles.size shouldBe 1
-        models.TestHelper.assertEqualsFile("/example-union-types-play-23.txt", sourceFiles.head.contents)
-      }
-    }
-  }
-
-  it("generates expected code for ning client") {
-    Ning18ClientGenerator.invoke(InvocationForm(service = service)) match {
-      case Left(errors) => fail(errors.mkString(", "))
-      case Right(sourceFiles) => {
-        sourceFiles.size shouldBe 1
-        models.TestHelper.assertEqualsFile("/example-union-types-ning-client.txt", sourceFiles.head.contents)
+        models.TestHelper.assertEqualsFile("/example-union-types-play-24.txt", sourceFiles.head.contents)
       }
     }
   }
--- scala-generator/src/test/scala/models/generator/CollectionJsonDefaultsSpec.scala
+++ scala-generator/src/test/scala/models/generator/CollectionJsonDefaultsSpec.scala
@@ -1,8 +1,7 @@
 package scala.generator
 
 import com.bryzek.apidoc.generator.v0.models.InvocationForm
-import scala.models.Play23ClientGenerator
-import scala.models.ning.Ning18ClientGenerator
+import scala.models.Play24ClientGenerator
 
 import models.TestHelper
 import org.scalatest.{FunSpec, Matchers}
@@ -23,22 +22,12 @@ class CollectionJsonDefaultsSpec extends FunSpec with Matchers {
     models.TestHelper.assertEqualsFile("/generators/collection-json-defaults-user-patch-case-class.txt", code)
   }
 
-  it("generates expected code for play 2.3 client") {
-    Play23ClientGenerator.invoke(InvocationForm(service = models.TestHelper.collectionJsonDefaultsService)) match {
+  it("generates expected code for play 2.4 client") {
+    Play24ClientGenerator.invoke(InvocationForm(service = models.TestHelper.collectionJsonDefaultsService)) match {
       case Left(errors) => fail(errors.mkString(", "))
       case Right(sourceFiles) => {
         sourceFiles.size shouldBe 1
-        models.TestHelper.assertEqualsFile("/generators/collection-json-defaults-play-23.txt", sourceFiles.head.contents)
-      }
-    }
-  }
-
-  it("generates expected code for ning client") {
-    Ning18ClientGenerator.invoke(InvocationForm(service = models.TestHelper.collectionJsonDefaultsService)) match {
-      case Left(errors) => fail(errors.mkString(", "))
-      case Right(sourceFiles) => {
-        sourceFiles.size shouldBe 1
-        models.TestHelper.assertEqualsFile("/generators/collection-json-defaults-ning-client.txt", sourceFiles.head.contents)
+        models.TestHelper.assertEqualsFile("/generators/collection-json-defaults-play-24.txt", sourceFiles.head.contents)
       }
     }
   }
